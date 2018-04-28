155d3350d5129b1b38bb0f73fc5bf0f1a330099d
Nicolas <niki.jullien@gmail.com>
2018-04-28 13:25:36 +0200

    Derniers petits réglages avant ajout scrapping sur le flask

--- App/views.py
+++ App/views.py
@@ -14,9 +14,17 @@ def Main_views(app):
         if request.method == 'POST':
             form = SearchForm(request.form)
             if form.validate_on_submit():
-                return redirect('/app/result/pl={}&ph={}&sl={}&sh={}&rn={}'.format(str(form.price_low.data), form.price_high.data, form.area_low.data, form.area_high.data, form.room_number.data))
+                if 0 < form.price_low.data < form.price_high.data and 0 < form.area_low.data < form.area_high.data and \
+                                form.room_number.data > 0:
+                    return redirect('/app/result/pl={}&ph={}&sl={}&sh={}&rn={}'.format(form.price_low.data,
+                                                                                       form.price_high.data,
+                                                                                       form.area_low.data,
+                                                                                       form.area_high.data,
+                                                                                       form.room_number.data))
+                else:
+                    return render_template('app_form.html', form=form, error=True)
             else:
-                return render_template('app_form.html', form=form)
+                return render_template('app_form.html', form=form, error=False)
         else:
             return render_template('show_results.html')
 
